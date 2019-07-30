7dbb9219d77e1ae84ac70c415df30e1612ae9994
Vova Yakovenko <Uladzimir.Yakavenka@itechart-group.com>
2019-07-30 17:13:21 +0300

    code performance

--- index.js
+++ index.js
@@ -1,61 +1,57 @@
-var AWS = require("aws-sdk");
-var FS = require("fs");
+const AWS = require("aws-sdk");
 const imagemin = require('imagemin');
 const imageminJpegtran = require('imagemin-jpegtran');
 const imageminPngquant = require('imagemin-pngquant');
 
 exports.handler = (event, context, callback) => {
-    var s3 = new AWS.S3();
-    var sourceBucket = "test.img.lambda";
+    const s3 = new AWS.S3();
+    const sourceBucket = "test.img.lambda";
+    const objectKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, '%20'));
 
-    var objectKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, '%20'));
-
-    var getObjectParams = {
+    const getObjectParams = {
         Bucket: sourceBucket,
         Key: objectKey,
     };
 
+    const isOptimized = tag => tag.TagSet[0] && tag.TagSet[0].Key === "optimized";
+
+    const putObject = (files, contentType) => {
+        s3.putObject({
+            Body: files,
+            Bucket: sourceBucket,
+            ContentType: contentType,
+            Key: objectKey,
+            Tagging: "optimized=true"
+        }).promise()
+            .then(data => console.log("S3 compressed object upload successful."))
+            .catch(err => console.log(err, err.stack));
+    };
+
+    const getObjectTagging = (data, getObjectParams) => {
+        s3.getObjectTagging(getObjectParams)
+            .promise()
+            .then(dataTag => {
+                if (isOptimized(dataTag)) {
+                    console.log("Image already optimized");
+                } else {
+                    (async () => {
+                        const files = await imagemin.buffer(data.Body, {
+                            plugins: [
+                                imageminJpegtran(),
+                                imageminPngquant({
+                                    quality: [0.6, 0.8]
+                                })
+                            ]
+                        });
+                        putObject(files, data.ContentType);
+                    })();
+                }
+            })
+            .catch(err => console.log(err));
+    };
+
     s3.getObject(getObjectParams)
         .promise()
-        .then(data => {
-            s3.getObjectTagging(getObjectParams)
-                .promise()
-                .then(dataTag => {
-                    if (dataTag.TagSet[0] && dataTag.TagSet[0].Key === "optimized") {
-                        console.log("image already optimized");
-                    } else {
-                        (async () => {
-                            const files = await imagemin.buffer(data.Body, {
-                                plugins: [
-                                    imageminJpegtran(),
-                                    imageminPngquant({
-                                        quality: [0.6, 0.8]
-                                    })
-                                ]
-                            });
-
-                            s3.putObject({
-                                Body: files,
-                                Bucket: sourceBucket,
-                                ContentType: data.ContentType,
-                                Key: objectKey,
-                                Tagging: "optimized=true"
-                            }).promise()
-                                .then(data => {
-                                    console.log("S3 compressed object upload successful.");
-                                })
-                                .catch(err => {
-                                    console.log(err, err.stack);
-                                });
-                        })();
-                    }
-                })
-                .catch(err => {
-                    console.log(err);
-                });
-        })
-        .catch(err => {
-            console.log(getObjectParams);
-            console.log(err);
-        });
+        .then(data => getObjectTagging(data, getObjectParams))
+        .catch(err => console.log(err));
 };
